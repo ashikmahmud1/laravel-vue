ce1d47b1b62564f4fd9b580c1b451556122c29a0
Ashik Mahmud <ashikmahmud@Ashiks-MacBook-Pro.local>
2021-06-28 10:32:56 +0600

    Post table setup for api

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,16 +9,15 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-
-    <!-- Scripts -->
-    <script src="{{ mix('/js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script src="{{ mix('/js/app.js') }}" defer></script>
 
 </head>
 <body class="antialiased">
